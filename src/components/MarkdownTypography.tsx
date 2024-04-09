eec875c1c49390041f54c709a00a2e2bf9a02b31
Max Gordon <max@gforge.se>
2024-04-09 22:43:52 +0200

    Refactor MarkdownTypography component to handle empty lines and add left alignment

--- src/components/MarkdownTypography.tsx
+++ src/components/MarkdownTypography.tsx
@@ -5,25 +5,35 @@ import { Typography, TypographyProps } from '@mui/material';
  */
 export const MarkdownTypography = ({ content }: { content: string }) => (
     <>
-        {content.split('\n\n').map((line, index) => {
-            let variant: TypographyProps['variant'] = 'body2';
-            let marginLeft = '0px';
-            if (line.startsWith('## ')) {
-                variant = 'h6';
-                marginLeft = '8px';
-            } else if (line.startsWith('### ')) {
-                variant = 'subtitle1';
-                marginLeft = '16px';
-            } else if (line.startsWith('#### ')) {
-                variant = 'subtitle2';
-                marginLeft = '24px';
-            }
+        {content
+            .split('\n')
+            .map((line) => line.trim())
+            .map((line, index) => {
+                let variant: TypographyProps['variant'] = 'body2';
+                let marginLeft = '0px';
+                if (line.startsWith('## ')) {
+                    variant = 'h6';
+                    marginLeft = '8px';
+                } else if (line.startsWith('### ')) {
+                    variant = 'subtitle1';
+                    marginLeft = '16px';
+                } else if (line.startsWith('#### ')) {
+                    variant = 'subtitle2';
+                    marginLeft = '24px';
+                }
+                if (line === '') {
+                    return <br key={index} />;
+                }
 
-            return (
-                <Typography variant={variant} key={index} sx={{ marginLeft }}>
-                    {line}
-                </Typography>
-            );
-        })}
+                return (
+                    <Typography
+                        variant={variant}
+                        key={index}
+                        sx={{ marginLeft, textAlign: 'left' }}
+                    >
+                        {line}
+                    </Typography>
+                );
+            })}
     </>
 );
