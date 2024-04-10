2a05505eef14285f5d6698ac66ddddc7e11a9f65
Max Gordon <max@gforge.se>
2024-04-10 19:53:35 +0200

    Minor index update

--- src/components/NoteList/index.ts
+++ src/components/NoteList/index.ts
@@ -1,2 +1,4 @@
 // @index(['./*/index.ts(|x)', './*.ts(|x)', '!./*.stories.ts(|x)', '!./NoteListRow.tsx'], f => `export * from '${f.path.replace(/\/index$/, "")}';`)
+export * from './ChooseLanguage';
 export * from './NoteList';
+export * from './types';
